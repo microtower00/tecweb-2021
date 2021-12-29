315d7d0ab6d36f09c1412de71a30423cce6c7252
microtower00 <microtower00@gmail.com>
2021-12-29 18:29:51 +0100

    Creazione e implementazione funzione per nav dinamico

--- mappa.php
+++ mappa.php
@@ -3,9 +3,6 @@ session_start();
 require_once "utils.php";
 
 $pagina = file_get_contents("mappa.html");
-$find = array("['LinkDashboard']","['LinkLogin']");
-$replaceDashboard = Utils::checkPriv()?"<a class='right' href='dashboard.php'>Dasboard Admin</a>":"";
-$replaceLogin = isset($_SESSION['Privilegi'])?"<a class='right' href='logout.php'>Logout</a>":"<a class='right' href='login.php'>Login</a>";
-$replace = array($replaceDashboard,$replaceLogin);
-echo str_replace($find, $replace, $pagina);
+$curPageName = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1); 
+echo str_replace("[Menu]",Utils::buildNav($curPageName),$pagina);
 ?>
--- utils.php
+++ utils.php
@@ -14,6 +14,53 @@ class Utils{
             return $_SESSION['Privilegi']=="1";
      }
 
+     public static function buildNav($page){
+        $find = array("['attrIndex']","['attrChisiamo']","['attrComprensorio']","['attrMappa']","['attrShop']","['attrComeraggiungerci']","['LinkLogin']","['LinkDashboard']");
+        $replace = array("href='index.php'","href='chisiamo.php'","href='comprensorio.php'","href='mappa.php'","href='shop.php'","href='comeRaggiungerci.php'","<a href='login.php' class='right'>Login</a>","");
+        $data = "<nav id='menu'>
+                    <a ['attrIndex']>Home</a>
+                    <a ['attrChisiamo']>Chi siamo</a>
+                    <a ['attrComprensorio']>Il nostro Comprensorio</a>
+                    <a ['attrMappa']>Mappa delle piste</a>
+                    <a ['attrShop']>Compra skipass</a>
+                    <a ['attrComeraggiungerci']>Come raggiuncerci</a>
+                    ['LinkLogin']
+                    ['LinkDashboard']
+                </nav>";
+        switch ($page){
+            case "index.php":
+                $replace[0] = "class = 'active'";
+                break;
+            case "chisiamo.php":
+                $replace[1] = "class = 'active'";
+                break;
+            case "comprensorio.php":
+                $replace[2] = "class = 'active'";
+                break;
+            case "mappa.php":
+                $replace[3] = "class = 'active'";
+                break;
+            case "shop.php":
+                $replace[4] = "class = 'active'";
+                break;
+            case "comeRaggiungerci.php":
+                $replace[5] = "class = 'active'";
+                break;
+            case "login.php":
+                $replace[6] = "<a class='active right'>Login</a>";
+                break;
+        }
+
+        if(isset($_SESSION['Privilegi'])){
+            $replace[6] = "<a href='logout.php' class='right'>Logout</a>";
+            if(Utils::checkPriv()){
+                $replace[7]="<a href='dashboard.php' class='right'>Dashboard Admin</a>";
+            }
+        }
+            $data = str_replace($find,$replace,$data);
+            return $data;
+     }
+
 }
 
 ?>
