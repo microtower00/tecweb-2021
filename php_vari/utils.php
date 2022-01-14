ce9173ecb5e0b8b7d8f1b4d2f8dc55d0e341569a
riccardo-contin <ricky11.contin@gmail.com>
2022-01-14 17:26:02 +0100

    Fix vari

--- php_vari/utils.php
+++ php_vari/utils.php
@@ -30,7 +30,7 @@ class Utils{
         $find = array("['attrIndex']","['attrChisiamo']","['attrComprensorio']","['attrMappa']","['attrShop']","['attrComeraggiungerci']","['LinkLogin']","['LinkDashboard']");
         $replace = array("href='index.php'","href='chisiamo.php'","href='comprensorio.php'","href='mappa.php'","href='shop.php'","href='comeRaggiungerci.php'","<li class='right'><a id='login' href='login.php' class='right'>Login</a></li>","");
         $data = "<nav id='menu'>
-                <h1>Valle Bianca Ski</h1>
+                <h2>Valle Bianca Ski</h2>
                 <div class='pulsanteMenu' onclick='toggleMenu()'>
                     <a href='#'>
                         <span class='barra'></span>
@@ -39,12 +39,12 @@ class Utils{
                     </a>
                 </div>
                     <ul>
-                        <li><a id='home'['attrIndex']>Home</a></li>
+                        <li><a id='home' ['attrIndex']>Home</a></li>
                         <li><a id='info' ['attrChisiamo']>Chi siamo</a></li>
-                        <li><a id='comprensorio'['attrComprensorio']>Il nostro Comprensorio</a></li>
-                        <li><a id='mappa'['attrMappa']>Mappa delle piste</a></li>
-                        <li><a id='shop'['attrShop']>Compra skipass</a></li>
-                        <li><a id='bus'['attrComeraggiungerci']>Come raggiuncerci</a></li>
+                        <li><a id='comprensorio' ['attrComprensorio']>Il nostro Comprensorio</a></li>
+                        <li><a id='mappaIcon' ['attrMappa']>Mappa delle piste</a></li>
+                        <li><a id='shop' ['attrShop']>Compra skipass</a></li>
+                        <li><a id='bus' ['attrComeraggiungerci']>Come raggiuncerci</a></li>
                             ['LinkLogin']
                             ['LinkDashboard']
                         
@@ -90,7 +90,8 @@ class Utils{
         $scriptMenu="<script src='js/menu.js'></script>";
         $cssTablet="<link rel='stylesheet' type='text/css' media='screen and (min-width: 601px) and (max-width: 1090px), only screen and (min-width: 601px) and (max-width:1090px)' href='css/tablet.css' />";
         $cssMini="<link rel='stylesheet' type='text/css' media='screen and (max-width: 600px), only screen and (max-width:600px)' href='css/mini.css' />";
-        return $css."\n".$cssTablet."\n".$cssMini."\n".$scriptMenu;
+        $icon="<link rel='shortcut icon' type='image/png' href='icon/icons8-montagna-48.png' />";
+        return $css."\n".$cssTablet."\n".$cssMini."\n".$scriptMenu."\n".$icon;
     }
 
     public static function skipNavBtn($html){
