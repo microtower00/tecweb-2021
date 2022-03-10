dcfda264769fd0cc47ae2b5b44c8a554e445a22e
riccardo-contin <ricky11.contin@gmail.com>
2022-03-10 16:11:27 +0100

    fix

--- js/modificaComprensorio.js
+++ js/modificaComprensorio.js
@@ -4,6 +4,7 @@ function nascondiForm() {
     document.getElementById('impianti-chiusi').style.display="none";
     document.getElementById('impianti-aperti').style.display="none";
     document.getElementById('cambia-descrizione').style.display="none";
+    document.getElementById('cambiaD').disabled=true;
 }
 
 function mostraFieldset(idInput) {
@@ -17,6 +18,27 @@ function mostraFieldset(idInput) {
         document.getElementById('impianti-aperti').style.display="none";
         document.getElementById('cambia-descrizione').style.display="none";
         document.getElementById(idInput).style.display="block";
+
+        document.getElementById(idInput).focus();
     }
-    
+}
+
+function checkDescrizione(){
+    var elem = document.getElementById("nuova-descrizione");
+
+    if (elem.value == "") {
+        elem.focus();
+        document.getElementById('feedback').innerHTML = "Descrizione mancante";
+        document.getElementById('feedback').style.display="block";
+        document.getElementById('cambiaD').disabled=true;
+    } else if (elem.value.length > 500) {
+        elem.focus();
+        document.getElementById('feedback').innerHTML = "Descrizione troppo lunga";
+        document.getElementById('feedback').style.display="block";
+        document.getElementById('cambiaD').disabled=true;
+    } else {
+        document.getElementById('feedback').innerHTML = "";
+        document.getElementById('cambiaD').disabled=false;
+        document.getElementById('cambiaD').focus();
+    }
 }
